7c1a0a9814c8df37e79b9a49ab2a43dee4df07c4
ZainaZaben <103901228+ZainaZaben@users.noreply.github.com>
2022-11-05 18:12:44 +0200

    Add files via upload

---
+++ puzzel/src/main/java/com/mycompany/puzzel/PuzzelClass.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.puzzel;
+
+
+public class PuzzelClass {
+    
+   private int value;
+    
+    public PuzzelClass(int value)
+    {
+        this.value = value;
+    }
+
+    PuzzelClass() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+   
+    public int getValue()
+    {
+        return value;
+    }
+}
